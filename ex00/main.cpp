b2f629519de3f466b0237b08805e943beb16e94b
sdxsd <willmaguire@icloud.com>
2023-08-08 20:36:51 +0200

    Cool shit.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,16 +2,17 @@
 #include "whatever.hpp"
 
 int main(void) {
-	int x = 5, y = 15;
-
-	std::cout << min(1, 10) << std::endl;
-	std::cout << max(11, 10) << std::endl;
-
-	std::cout << "x == " << x << std::endl;
-	std::cout << "y == " << y << std::endl;
-	std::cout << "Swapping..." << std::endl;
-	swap(x, y);
-	std::cout << "x == " << x << std::endl;
-	std::cout << "y == " << y << std::endl;
-
+  int a = 2;
+  int b = 3;
+  swap(a, b);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+  std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+  return 0;
 }

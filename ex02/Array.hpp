f5f2aafef96e71485ad3d5a44f23f942aee21dc3
sdxsd <willmaguire@icloud.com>
2023-12-05 15:36:44 +0100

    Fixed compile issue.

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -15,12 +15,12 @@ template <typename T> class Array {
 				delete []elements;
 		}
 		Array(unsigned int n) {
-			elements = new T[n]; // Need to add alloc checker.
+			elements = new T[n];
 			length = n;
 		}
 		Array(const Array& copy) {
 			elements = new T[copy.length];
-			for (int i = 0; i < copy.length; i++)
+			for (unsigned int i = 0; i < copy.length; i++)
 				elements[i] = copy.elements[i];
 			length = copy.length;
 		}
@@ -30,7 +30,7 @@ template <typename T> class Array {
 				if (length > 0)
 					delete []elements;
 				this->elements = new T[copy.length];
-				for (int i = 0; i < copy.length; i++)
+				for (unsigned int i = 0; i < copy.length; i++)
 					this->elements[i] == copy.elements[i];
 				this->length = copy.length;
 			}
